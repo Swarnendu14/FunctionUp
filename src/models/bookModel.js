fe45a6bf52c8e78044e1127a8501b1fc6028ca07
Swarnendu14 <swarnendustps@gmail.com>
2023-05-03 04:00:36 +0530

    Ref and populate Assignment is done

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,15 +3,22 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
         type: ObjectId,
-        ref: "LibraryAuthor"
+        ref: "newAuthor",
+        required:true,
+        isValid: true
     }, 
     price: Number,
-    rating: Number
-
+    rating: Number,
+    publisher:{
+        type: ObjectId,
+        ref: "newPublisher",
+        required:true,
+        isValid: true
+    }
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('newBook', bookSchema)
---
+++ src/models/publisherModel.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+const publisherSchema=new mongoose.Schema({
+    name:{type:String,
+        required:true},
+    headQuarter:String,
+    isHardCover:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports=mongoose.model('newPublisher',publisherSchema);
