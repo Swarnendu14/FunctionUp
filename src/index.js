addea52829b8b97659a55f104cd016f72df3dde0
Swarnendu14 <swarnendustps@gmail.com>
2023-05-10 00:09:43 +0530

    Authentication Assignment done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://swarnenduktpp:Rq0bKY4NZeTmPE0F@cluster0.qntniml.mongodb.net/swarno-auth-1?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
+    firstName:{type: String,
+        required:true},
     lastName: String,
     mobile: {
         type: String,
-
         required: true
     },
     emailId: String,
-    password: String,
-    gender: {
+    password:{type: String,
+        required:true},
+    gender: { 
         type: String,
         enum: ["male", "female", "other"]
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
     },
     age: Number,
 }, { timestamps: true });
