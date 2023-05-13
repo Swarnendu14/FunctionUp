3643d7c32c23555f96061ddab51b2f2c723ac0d8
Swarnendu14 <swarnendustps@gmail.com>
2023-05-13 22:46:39 +0530

    try-catch-status assignment done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,32 +3,42 @@ const userModel = require("../models/userModel");
 
 
 const createUser = async function (req,res) {
+try{
   let data = req.body;
   let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
+  res.status(201).send({ msg: savedData });
+}
+catch (err) {
+  res.status(500).send(err.message);
+}
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if(!user){
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-  }
-  else{
-    let id=user._id;
-    let token=jwt.sign({_id:id},'swarnenduSecretMsg');
-    res.setHeader("x-auth-token", token);
-    res.send({status:"true",data:{Token:token}});
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user) {
+        return res.status(404).send({
+            status: false,
+            msg: "username or the password is not corerct",
+        });
+    } else {
+        let id = user._id;
+        let token = jwt.sign({ _id: id }, 'MySecretMsg');
+        res.setHeader("x-auth-token", token);
+        res.status(200).send({ status: "true", data: { Token: token } });
+    }
+  } 
+  catch (err) {
+    res.status(500).send(err.message);
   } 
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-auth-token"];
+  try{
+    let token = req.headers["x-auth-token"];
   
   let decodedToken = jwt.verify(token, "swarnenduSecretMsg");
   if (!decodedToken){
@@ -36,21 +46,34 @@ const getUserData = async function (req, res) {
   }
   let userId = req.params.userId;
   let userDetails=await userModel.findById(userId);
-  res.send({ status: true,token: decodedToken, data: userDetails });
-  
+  res.status(200).send({ status: true,token: decodedToken, data: userDetails });
+}
+ catch(err){
+  res.status(500).send(err.message);
+ } 
 };
 
 const updateUser = async function (req, res) {
-  let userId = req.params.userId;
+  try{
+    let userId = req.params.userId;
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-  res.send({ status: "Updated User", data: updatedUser });
+  res.status(200).send({ status: "Updated User", data: updatedUser });
+}
+catch(err){
+  res.status(500).send(err.message);
+}
 }
 
 let deleteUser=async (req,res)=>{
-  let userId = req.params.userId;
+  try{
+    let userId = req.params.userId;
   let deletedUser = await userModel.findOneAndUpdate({ _id: userId },{isDeleted:true},{new:true});
-  res.send({ status:" Deleted User", data: deletedUser});
+  res.status(200).send({ status:" Deleted User", data: deletedUser});
+}
+catch (err) {
+  res.status(500).send(err.message);
+}
 }
 
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,44 @@
 const jwt=require('jsonwebtoken')
 const userModel=require('../models/userModel')
 let headerCheck=(req,res,next)=>{
+  try{
     let token = req.headers["x-auth-token"];
   if (!token){ 
-    return res.send({ status: false, msg: "token must be present in Request Header" });
+    return res.status(404).send({ status: false, msg: "token must be present in Request Header" });
   }
   next();
 }
+catch(err){
+  return res.status(500).send({msg:err.message});
+}
+}
 let userCheck= async (req,res,next)=>{
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  if (!user) {
-    return res.send({status:false,ErrorMsg:"No such user exists in my DataBase, Authentication Fail"});
+  try{
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).send({status:false,ErrorMsg:"No such user exists in my DataBase, Authentication Fail"});
+    }
+    next();
   }
-  next();
+  catch(err){
+    return res.status(500).send({msg:err.message});
+  }
 }
 
 let checkLoginUser=async (req,res,next)=>{
-    let userId = req.params.userId;
+    try{
+      let userId = req.params.userId;
     let token = req.headers["x-auth-token"];
     let loginId=jwt.verify(token,'swarnenduSecretMsg');
     if(loginId._id!=userId){
-       return res.send({status:false,ErrorMsg:"You are not the login User, Access Denied"})
+       return res.status(404).send({status:false,ErrorMsg:"You are not the login User, Access Denied"})
     }
     next();
+  }
+  catch(err){
+    return res.status(500).send({msg:err.message});
+  }
 }
 
 module.exports={headerCheck,userCheck,checkLoginUser}
